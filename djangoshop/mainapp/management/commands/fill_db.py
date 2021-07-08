c4e9eda31061438e699ca30e2ecb2fc9325e6050
Sergey <sgb128@mail.ru>
2021-07-08 20:00:22 +0300

    Homework for lesson 4 is start.

---
+++ djangoshop/mainapp/management/commands/fill_db.py
@@ -0,0 +1,35 @@
+import os
+import json
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
+
+JSON_PATH = 'mainapp/jsons'
+
+def load_from_json(file_name):
+    with open(os.path.join(JSON_PATH, file_name + '.json'), mode='r', encoding='UTF-8') as infile:
+        return json.load(infile)
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        categoties = load_from_json('categories')
+
+        ProductCategory.objects.all().delete()
+        for category in categoties:
+            new_category = ProductCategory(**category)
+            new_category.save()
+
+        products = load_from_json('products')
+
+        Product.objects.all().delete()
+        for product in products:
+            category_name = product['category']
+            _category = ProductCategory.objects.get(name=category_name)
+            product['category'] = _category
+            new_category = Product(**product)
+            new_category.save()
+
+        super_user = ShopUser.objects.create_superuser(username='admin', email='admin@djangoshop', password='123', age='30')

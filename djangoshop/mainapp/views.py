6895017006210150dcdac97380b4367b5cf6eaed
Sergey <sgb128@mail.ru>
2021-07-09 16:52:55 +0300

    Homework for lesson 5 is start.

--- djangoshop/mainapp/views.py
+++ djangoshop/mainapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+from mainapp.models import ProductCategory
 
-def products(request):
-    return render(request, 'mainapp/products.html')
+
+def products(request, pk=None):
+    title = 'Продукты/каталог'
+
+    links_menu = ProductCategory.objects.all()
+
+    context = {
+        'title': title,
+        'links_menu': links_menu,
+    }
+
+    return render(request=request, template_name='mainapp/products.html', context=context)

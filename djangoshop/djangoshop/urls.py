33b4fba2903547c6ac0407d53feecbb89dad02a3
Sergey <sgb128@mail.ru>
2021-07-11 14:10:29 +0300

    Homework for lesson 5 is done.

--- djangoshop/djangoshop/urls.py
+++ djangoshop/djangoshop/urls.py
@@ -23,8 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('contacts/', contacts, name='contacts'),
-    path('products/', include('mainapp.urls')),
+    path('products/', include('mainapp.urls', namespace='products')),
     path('auth/', include('authapp.urls', namespace='auth')),
+    path('basket/', include('basketapp.urls', namespace='basket'))
 ]
 
 if settings.DEBUG:
--- djangoshop/mainapp/urls.py
+++ djangoshop/mainapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import products
 
-# app_name = 'products'
+app_name = 'products'
 
 urlpatterns = [
     path('', products, name='products'),
+    path('category/<int:pk>/', products, name='category')
 ]
